59adafeeadc596aebbf375a6568e9b041063f4bb
zhouhaofeng <wb.zhouhaofeng@mesg.corp.netease.com>
2023-12-04 16:46:55 +0800

    init source

---
+++ MyFPSProject/Source/MyFPSProject/MyFPSProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyFPSProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFPSPROJECT_API AMyFPSProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
